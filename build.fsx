9f6ba90c084b31e8bc51850b8e9bea47db59e272
Volcomix <volcomix@gmail.com>
2019-01-17 22:46:39 +0100

    Write merged challenge file in the output directory

--- build.fsx
+++ build.fsx
@@ -12,49 +12,52 @@ open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
+let buildDir = "build"
+
+let parseLine (opens, lines, isModule) line =
+    match line with
+    | "" when isModule && List.head lines = "    " -> opens, lines, isModule
+    | _ when String.startsWith "open " line -> Set.add line opens, lines, isModule
+    | _ when String.startsWith "module " line -> opens, line + " =" :: lines, true
+    | _ when isModule -> opens, "    " + line :: lines, isModule
+    | _ -> opens, line :: lines, isModule
+
+let parseFile (opens, lines) file =
+    let opens, lines, _ =
+        File.read file
+        |> Seq.fold parseLine (opens, "" :: lines, false)
+    opens, lines
+
 Target.create "Clean" (fun _ ->
     !! "src/**/bin"
     ++ "src/**/obj"
+    ++ buildDir
     |> Shell.cleanDirs)
 
 Target.create "Build" (fun _ ->
-    !! "src/**/*.*proj"
+    !! "src/Challenge/*.*proj"
+    |> Seq.iter (DotNet.build id))
+
+Target.create "BuildTest" (fun _ ->
+    !! "src/Challenge.Tests/*.*proj"
     |> Seq.iter (DotNet.build id))
 
 Target.create "Merge" (fun _ ->
     let files =
         Xml.read true "src/Challenge/Challenge.fsproj" "" "" "//Compile/@Include"
         |> Seq.map ((</>) "src/Challenge")
-
     Trace.logItems "Merging: " files
-    Trace.traceLine ()
-
-    let opens, lines =
-        files
-        |> Seq.fold (fun (opens, lines) file ->
-            let opens, lines, _ =
-                File.read file
-                |> Seq.fold (fun (opens, lines, isModule) line ->
-                    match line with
-                    | "" when isModule && List.head lines = "    " -> opens, lines, isModule
-                    | _ when String.startsWith "open " line -> Set.add line opens, lines, isModule
-                    | _ when String.startsWith "module " line -> opens, line + " =" :: lines, true
-                    | _ when isModule -> opens, "    " + line :: lines, isModule
-                    | _ -> opens, line :: lines, isModule) (opens, lines, false)
-            opens, "" :: lines) (Set.empty, [])
-
-    Set.toSeq opens
-    |> Trace.logItems "Opens: "
-
-    Trace.trace ""
-
+    let opens, lines = Seq.fold parseFile (Set.empty, []) files
+    Shell.mkdir buildDir
     List.rev lines
-    |> Trace.logItems "Lines: ")
+    |> Seq.append opens
+    |> File.writeNew (buildDir </> "Challenge.fs"))
 
 Target.create "All" ignore
 
-"Clean"
-  ==> "Build"
-  ==> "All"
+"Clean" ==> "All"
+"Build"  ==> "All"
+"BuildTest" ==> "All"
+"Merge" ==> "All"
 
 Target.runOrDefault "All"
